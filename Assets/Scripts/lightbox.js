7245d1d05c58ca8da20dc9b56c94af5133b733c4
whix100 <whix100@gmail.com>
2020-06-17 21:24:27 -0400

    Updated Projects
    Updated the Projects page and added a MMMM page

---
+++ Assets/Scripts/lightbox.js
@@ -0,0 +1,7 @@
+function openLightBox() {
+    document.getElementById("lightbox").style.display = "block";
+}
+
+function closeLightBox() {
+    document.getElementById("lightbox").style.display = "none";
+}
